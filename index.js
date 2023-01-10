02a5ca061f28fd2e2b77f3efd725cbfc7850f418
dominikpollak <dominikpollak17@gmail.com>
2023-01-10 19:11:01 +0100

    final touches

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const heading = document.getElementById('heading')
 const subheading = document.getElementById('subheading')
 const images = document.querySelectorAll('.image')
 const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-// const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
 const arrowRight = document.getElementById('arrow-right')
 const arrowLeft = document.getElementById('arrow-left')
 const arrows = document.getElementById('arrows')
@@ -374,7 +373,7 @@ if (!mouseMoved) {
                 carousel.dataset.percentage = (((imageWidth + 24) * (i + 0.5)) * -1) / 78.876811594203
                 console.log(innerWidth)
                 carousel.animate({
-                    transform: `translate(${((imageWidth + 24) * (i + (innerWidth / 1180.32786885))) * -1}px, -45%)`
+                    transform: `translate(${((imageWidth + 24) * (i + (innerWidth/1190.08264463))) * -1}px, -45%)`
                 }, { duration: 1200, fill: 'forwards', easing: "cubic-bezier(0, 0, 0.58, 1)" })
             }
 
